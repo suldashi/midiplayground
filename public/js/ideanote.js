632036025d8c63b45cf58119eebdb60c388318bd
ermirsuldashi <ermirsuldashi@gmail.com>
2017-05-29 18:03:39 -0500

    implemented a basic version of sendMessage and incomingMessage

--- public/js/ideanote.js
+++ public/js/ideanote.js
@@ -91,6 +91,66 @@ class Sequencer {
         this.scheduler = new WebAudioScheduler(audioContext);
         this.noteSequence = new Array();
         this.channelNumber = channel;
+        this.noteSequence.push(new SequenceNote(0, {
+            type: "noteon",
+            note: {
+                number: 60
+            }
+        }));
+        this.noteSequence.push(new SequenceNote(0.1, {
+            type: "noteoff",
+            note: {
+                number: 60
+            }
+        }));
+        this.noteSequence.push(new SequenceNote(0.2, {
+            type: "noteon",
+            note: {
+                number: 60
+            }
+        }));
+        this.noteSequence.push(new SequenceNote(0.3, {
+            type: "noteoff",
+            note: {
+                number: 60
+            }
+        }));
+        this.noteSequence.push(new SequenceNote(0.4, {
+            type: "noteon",
+            note: {
+                number: 60
+            }
+        }));
+        this.noteSequence.push(new SequenceNote(0.5, {
+            type: "noteoff",
+            note: {
+                number: 60
+            }
+        }));
+        this.noteSequence.push(new SequenceNote(0.6, {
+            type: "noteon",
+            note: {
+                number: 60
+            }
+        }));
+        this.noteSequence.push(new SequenceNote(0.7, {
+            type: "noteoff",
+            note: {
+                number: 60
+            }
+        }));
+        this.noteSequence.push(new SequenceNote(0.8, {
+            type: "noteon",
+            note: {
+                number: 60
+            }
+        }));
+        this.noteSequence.push(new SequenceNote(0.9, {
+            type: "noteoff",
+            note: {
+                number: 60
+            }
+        }));
         this.sequenceLength = 1;
         this.recordingBuffer = [];
     }
@@ -114,15 +174,19 @@ class Sequencer {
         var sequence = this.noteSequence;
         if (typeof this.sink !== "undefined") {
             this.scheduler.start((e) => {
-                var offset = 0;
-                for (var j = 0; j < 50; j++) {
-                    for (var i in sequence) {
-                        this.scheduler.insert(e.playbackTime + sequence[i].playbackTime + offset, (e) => {
+                for (var i in sequence) {
+                    if (sequence[i].playbackTime == 0) {
+                        this.sink.receiveNote(sequence[i].note);
+                    }
+                    else {
+                        this.scheduler.insert(e.playbackTime + sequence[i].playbackTime, (e) => {
                             this.sink.receiveNote(e.args.note);
                         }, { note: sequence[i].note });
                     }
-                    offset += this.sequenceLength;
                 }
+                this.scheduler.insert(e.playbackTime + this.sequenceLength, (e) => {
+                    this.startPlaying();
+                });
             });
         }
     }
--- public/js/ideanote/sequencer.ts
+++ public/js/ideanote/sequencer.ts
@@ -20,56 +20,56 @@ class Sequencer {
 	    this.noteSequence = new Array<SequenceNote>();
 	    this.channelNumber = channel;
 
-	    // this.noteSequence.push(new SequenceNote(0,{
-	    // 	type:"noteon",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
-	    // this.noteSequence.push(new SequenceNote(0.1,{
-	    // 	type:"noteoff",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
-	    // this.noteSequence.push(new SequenceNote(0.2,{
-	    // 	type:"noteon",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
-	    // this.noteSequence.push(new SequenceNote(0.3,{
-	    // 	type:"noteoff",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
-	    // this.noteSequence.push(new SequenceNote(0.4,{
-	    // 	type:"noteon",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
-	    // this.noteSequence.push(new SequenceNote(0.5,{
-	    // 	type:"noteoff",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
-	    // this.noteSequence.push(new SequenceNote(0.6,{
-	    // 	type:"noteon",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
-	    // this.noteSequence.push(new SequenceNote(0.7,{
-	    // 	type:"noteoff",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
-	    // this.noteSequence.push(new SequenceNote(0.8,{
-	    // 	type:"noteon",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
-	    // this.noteSequence.push(new SequenceNote(0.9,{
-	    // 	type:"noteoff",
-	    // 	note:{
-	    // 		number:60
-    	// 	}}));
+	    this.noteSequence.push(new SequenceNote(0,{
+	    	type:"noteon",
+	    	note:{
+	    		number:60
+    		}}));
+	    this.noteSequence.push(new SequenceNote(0.1,{
+	    	type:"noteoff",
+	    	note:{
+	    		number:60
+    		}}));
+	    this.noteSequence.push(new SequenceNote(0.2,{
+	    	type:"noteon",
+	    	note:{
+	    		number:60
+    		}}));
+	    this.noteSequence.push(new SequenceNote(0.3,{
+	    	type:"noteoff",
+	    	note:{
+	    		number:60
+    		}}));
+	    this.noteSequence.push(new SequenceNote(0.4,{
+	    	type:"noteon",
+	    	note:{
+	    		number:60
+    		}}));
+	    this.noteSequence.push(new SequenceNote(0.5,{
+	    	type:"noteoff",
+	    	note:{
+	    		number:60
+    		}}));
+	    this.noteSequence.push(new SequenceNote(0.6,{
+	    	type:"noteon",
+	    	note:{
+	    		number:60
+    		}}));
+	    this.noteSequence.push(new SequenceNote(0.7,{
+	    	type:"noteoff",
+	    	note:{
+	    		number:60
+    		}}));
+	    this.noteSequence.push(new SequenceNote(0.8,{
+	    	type:"noteon",
+	    	note:{
+	    		number:60
+    		}}));
+	    this.noteSequence.push(new SequenceNote(0.9,{
+	    	type:"noteoff",
+	    	note:{
+	    		number:60
+    		}}));
 	    this.sequenceLength = 1;
 	    this.recordingBuffer = [];
 	}
@@ -99,20 +99,26 @@ class Sequencer {
 		if(typeof this.sink !== "undefined") {
 			this.scheduler.start((e) => {
 
-				var offset = 0;	
-				for (var j = 0; j<50;j++) {
+				//var offset = 0;	
+				//for (var j = 0; j<50;j++) {
 					for(var i in sequence) {
-						this.scheduler.insert(e.playbackTime + sequence[i].playbackTime + offset,(e) => {
-							this.sink.receiveNote(e.args.note);
-						},{note:sequence[i].note});
+						if(sequence[i].playbackTime==0) {
+							this.sink.receiveNote(sequence[i].note);
+						}
+						else {
+							this.scheduler.insert(e.playbackTime + sequence[i].playbackTime,(e) => {
+								this.sink.receiveNote(e.args.note);
+							},{note:sequence[i].note});
+						}
+						
 					} 
 
-					offset += this.sequenceLength;
-				}
+				//	offset += this.sequenceLength;
+				//}
 					
-				// this.scheduler.insert(e.playbackTime + this.sequenceLength,(e) => {
-				// 	this.startPlaying();
-				// })
+				this.scheduler.insert(e.playbackTime + this.sequenceLength,(e) => {
+					this.startPlaying();
+				})
 			});
 		}
 	}
